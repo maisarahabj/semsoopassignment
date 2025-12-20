001e824b8501049b1cc3372a6b3df5cb1e9ed7fb
Maisarah Ab J <msrh_aj@hotmail.com>
2025-12-21 05:13:53 +0800

    Szerina chan... mySQL & JDBC driver are connected now :-) xoxo

--- src/main/java/com/sems/util/DatabaseConnection.java
+++ src/main/java/com/sems/util/DatabaseConnection.java
@@ -4,10 +4,27 @@
  */
 package com.sems.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author maisarahabjalil
  */
 public class DatabaseConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/sems_db";
+        String user = "root";
+        String password = "Rockie.69";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("MySQL JDBC Driver is working!");
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
+    }
     
 }
