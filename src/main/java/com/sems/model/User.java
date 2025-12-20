ed9b7439559c6e3b51bb764c5f39cc6605daf2f0
Maisarah Ab J <msrh_aj@hotmail.com>
2025-12-21 07:20:28 +0800

    model > user.java done

--- src/main/java/com/sems/model/User.java
+++ src/main/java/com/sems/model/User.java
@@ -1,5 +1,7 @@
 package com.sems.model;
 
+import java.sql.Timestamp;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +12,56 @@ package com.sems.model;
  * @author maisarahabjalil
  */
 public class User {
+    // Private Fields from SQL
+    private int userId;
+    private String username;
+    private String passwordHash;
+    private String role; 
+    private Timestamp createdAt;
+    private boolean isActive;
+
+    // Constructors
+   
+    public User() {
+        this.isActive = true; 
+    }
     
+    public User(String username, String passwordHash, String role) {
+        this();
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public Timestamp getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
+
+    public boolean isIsActive() { return isActive; }
+    public void setIsActive(boolean isActive) { this.isActive = isActive; }
+
+    
+    // Checks if user is admin - used for Role-Based Access Control
+    public boolean isAdmin() {
+        return "admin".equalsIgnoreCase(this.role);
+    }
+
+    // Overriding toString 
+    @Override
+    public String toString() {
+        return "User [ID=" + userId + ", Username=" + username + ", Role=" + role + "]";
+    }
 }
+
--- src/main/java/com/sems/util/DatabaseConnection.java
+++ src/main/java/com/sems/util/DatabaseConnection.java
@@ -28,5 +28,3 @@ public class DatabaseConnection {
     }
     
 }
-
-///hi
