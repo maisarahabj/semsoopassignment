1cc016f23f2f3e113f68fdf171c96c37be0e5e66
Maisarah Ab J <msrh_aj@hotmail.com>
2025-12-23 12:35:00 +0800

    updated adminstudent. editing add status column

--- src/main/webapp/js/adminstudent.js
+++ src/main/webapp/js/adminstudent.js
@@ -1,7 +1,15 @@
+function openAddModal() {
+    document.getElementById('addStudentOverlay').style.display = 'flex';
+}
+
+function closeAddModal() {
+    document.getElementById('addStudentOverlay').style.display = 'none';
+}
+
 function showDeleteModal(id, fName, lName) {
     document.getElementById('modalStudentId').value = id;
-    document.getElementById('deleteMessage').innerHTML = 
-        "Are you sure you want to permanently remove student <strong>" + fName + " " + lName + "</strong> (ID: #" + id + ") from the system?";
+    document.getElementById('removeMessage').innerHTML = 
+        "Are you sure you want to permanently remove student <strong>" + fName + " " + lName + "</strong> (ID: #" + id + ")?";
     document.getElementById('deleteOverlay').style.display = 'flex';
 }
 
@@ -10,7 +18,31 @@ function closeDeleteModal() {
 }
 
 window.onclick = function(event) {
-    if (event.target == document.getElementById('deleteOverlay')) {
-        closeDeleteModal();
-    }
+    if (event.target == document.getElementById('deleteOverlay')) closeDeleteModal();
+    if (event.target == document.getElementById('addStudentOverlay')) closeAddModal();
 }
+
+// START -- admin pop up for view full profile 
+function showProfileModal(id, fName, lName, email, phone, address, dob, gpa) {
+    document.getElementById('viewFullName').innerText = fName + " " + lName;
+    document.getElementById('viewStudentId').innerText = "Student ID: #" + id;
+    document.getElementById('viewEmail').innerText = email;
+    document.getElementById('viewPhone').innerText = (phone !== "null" && phone !== "") ? phone : "Not Provided";
+    document.getElementById('viewDob').innerText = dob;
+    document.getElementById('viewGpa').innerText = parseFloat(gpa).toFixed(2);
+    document.getElementById('viewAddress').innerText = (address !== "null" && address !== "") ? address : "No address on file";
+
+    document.getElementById('profileOverlay').style.display = 'flex';
+}
+
+function closeProfileModal() {
+    document.getElementById('profileOverlay').style.display = 'none';
+}
+
+// Update the window click listener to handle the 3rd modal
+window.onclick = function(event) {
+    if (event.target == document.getElementById('deleteOverlay')) closeDeleteModal();
+    if (event.target == document.getElementById('addStudentOverlay')) closeAddModal();
+    if (event.target == document.getElementById('profileOverlay')) closeProfileModal();
+}
+// END -- admin pop up for view all profile
